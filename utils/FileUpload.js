9eb0f6b38360679bc2c7fcded5bda24546c91b49
ankit2807 <ankit.kushwaha2807@gmail.com>
2022-12-29 12:46:22 +0530

    file issue fixed

--- utils/FileUpload.js
+++ utils/FileUpload.js
@@ -1,9 +1,18 @@
 const multer = require("multer");
 const router = require('express').Router();
+const path = require('path')
 
 //upload logic
-const upload = multer({
-    dest: 'public',
+const storage = multer.diskStorage({
+    destination: 'public',
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '_' + Date.now()
+            + path.extname(file.originalname))
+    }
+});
+
+const Upload = multer({
+    storage: storage,
     limits: {
         fileSize: 1000000
     },
@@ -13,10 +22,26 @@ const upload = multer({
         }
         cb(undefined, true)
     }
-});
+})
+// const upload = multer({
+//     storage: multer.diskStorage({
+//         destination: function (req, file, cb) {
+//             cb(null, 'public')
+//         }
+//     }),
+//     // fileFilter: function(req, file, cb) {
+//     //     if (!file.originalname.match(/\.(jpg|jpeg|png|jfif)$/)) {
+//     //         return cb(new Error('Please upload a Image'))
+//     //     }
+//     //     cb(undefined, true)
+//     // },
+//     filename: function (req, file, cb) {
+//         cb(null, file.originalname + "-" + Date.now() + ".jpg")
+//     },
+// });
 
 //upload the image
-router.post('/', upload.array('image'), (req, res) => {
+router.post('/', Upload.array('image'), (req, res) => {
     res.status(200).json('Images uploaded')
 }, (error, req, res) => {
     res.status(400).send({ error: error.message })
